f292aa62808273534850b888a5d8aca5ea556ddc
xpertOfRock <106612134+xpertOfRock@users.noreply.github.com>
2023-12-30 12:36:47 +0200

    Update Student.java

--- src.main/java.sumdu.edu.ua/Student.java
+++ src.main/java.sumdu.edu.ua/Student.java
@@ -4,14 +4,18 @@ public class Student {
     private String name;
     private String surname;
     private String email;
+    private String group;
+    private String faculty;
 
     public Student() {
     }
 
-    public Student(String name, String surname, String email) {
+    public Student(String name, String surname, String email, String group, String faculty) {
         this.name = name;
         this.surname = surname;
         this.email = email;
+        this.group = group;
+        this.faculty = faculty;
     }
 
     public String getName() {
@@ -36,5 +40,21 @@ public class Student {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public String getFaculty() {
+        return faculty;
+    }
+
+    public void setFaculty(String faculty) {
+        this.faculty = faculty;
     }
 }
